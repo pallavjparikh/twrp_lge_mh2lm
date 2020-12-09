4f6d74dc217b6b6daa9996afea8f61a5cacbbecb
Pallav Parikh <45123301+pallavjparikh@users.noreply.github.com>
2020-12-09 18:20:17 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,3 +86,6 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+
+USE_RECOVERY_INSTALLER := true
+RECOVERY_INSTALLER_PATH := device/lge/mh2lm/installer
